4e1423cad3e6b196090364651ffbc3735c6ff6f5
OiNutter <willmckenzie@oinutter.co.uk>
2012-04-26 08:54:21 +0100

    Initial Commit
    Set up project dir

---
+++ README.md
@@ -0,0 +1,10 @@
+shift
+=======
+
+License
+-------
+
+Copyright 2012 Will McKenzie
+
+shift is licensed under the MIT License, please see the LICENSE file
+for more details.
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='shift',
+	  version='0.1',
+	  description='ADD DESCRIPTION HERE',
+	  author='Will McKenzie',
+	  author_email='will@oinutter.co.uk',
+	  packages=['shift'],
+	  package_dir={'shift': '<skeleton_project'},
+      package_data={},
+      requires:[]
+	  )
