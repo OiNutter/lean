2d220fc3091e3376f696500d622137b9f123c881
Will McKenzie <willmckenzie@oinutter.co.uk>
2013-01-08 11:18:26 +0000

    Tidy setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(name='Shift',
-	  version='0.1',
-	  description='Generic interface to multiple Python template engines - Tilt for Python',
-	  author='Will McKenzie',
-	  author_email='will@oinutter.co.uk',
-	  packages=['shift'],
-	  package_dir={'shift': 'shift'},
+      version='0.1',
+      description='Generic interface to multiple Python template engines - Tilt for Python',
+      author='Will McKenzie',
+      author_email='will@oinutter.co.uk',
+      packages=['shift'],
+      package_dir={'shift': 'shift'},
       package_data={},
       requires=['coffeescript','pyScss']
-	  )
+      )
